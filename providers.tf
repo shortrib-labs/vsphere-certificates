b03324facf6ed5f140402476729dd45fd7c0ce78
Chuck D'Antonio <cdantonio@pivotal.io>
2021-01-07 15:54:55 -0500

    Cleans up deprecated expression

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
   credentials = file("${local.directories.secrets}/gcp-homelab-service-account.json")
-  project     = "${var.gcp_project}"
+  project     = var.gcp_project
   region      = "us-east-4"
 }
